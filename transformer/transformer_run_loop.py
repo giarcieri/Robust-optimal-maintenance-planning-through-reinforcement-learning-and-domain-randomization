607d1b2efded4dc31b7392c57536efffd108029c
giarcieri <arcieri.1587763@studenti.uniroma1.it>
2022-07-22 15:19:01 +0200

    first tested version

---
+++ transformer/transformer_run_loop.py
@@ -0,0 +1,224 @@
+import random
+import time
+import pickle
+import os, sys
+
+from transformer_agent import *
+dir2 = os.path.abspath('')
+dir1 = os.path.dirname(dir2)
+if not dir1 in sys.path: sys.path.append(dir1)
+from environment.fractal_env_jax import *
+
+class ReplayBufferPO(object):
+    """A replay buffer for POMDPs that stores time-series of observations"""
+
+    def __init__(self, capacity, episode_horizon=50, window_length=5, gamma=0.995):
+        self.obs_buffer = jnp.zeros((capacity, episode_horizon))
+        self.action_buffer = jnp.zeros((capacity, episode_horizon))
+        self.reward_buffer = jnp.zeros((capacity, episode_horizon))
+        self.capacity = capacity
+        self.episode_horizon = episode_horizon
+        self.window_length = window_length
+        self.gamma = gamma
+
+    def push(self, obs, action, reward, episode, step):
+        self.size_buffer = min(episode+1, self.capacity)
+        episode = episode % self.capacity
+        self.obs_buffer = self.obs_buffer.at[episode, step].set(obs.squeeze())
+        self.action_buffer = self.action_buffer.at[episode, step].set(action.squeeze())
+        self.reward_buffer = self.reward_buffer.at[episode, step].set(reward.squeeze())
+
+    def sample(self, idx): 
+        obs_trajectory = self.obs_buffer[idx]
+        action_trajectory = self.action_buffer[idx]
+        reward_trajectory = self.reward_buffer[idx]
+        sub_windows = (
+            jnp.expand_dims(jnp.arange(self.window_length), 0) +
+            jnp.expand_dims(jnp.arange(self.episode_horizon + 1 - self.window_length), 0).T
+        )
+        # observations
+        obs_sliding_window = obs_trajectory[sub_windows] # shape (episode_horizon + 1 - window_length, window_length)
+        obs_tm1_trajectory = obs_sliding_window[:-1, :] # shape (episode_horizon - window_length, window_length)
+        obs_t_trajectory = obs_sliding_window[1:, :] # shape (episode_horizon - window_length, window_length)
+        # actions 
+        action_sliding_window = action_trajectory[sub_windows]
+        a_tm1_trajectory = action_sliding_window[:-1, :]
+        # rewards
+        rewards_sliding_window = reward_trajectory[sub_windows]
+        r_t_trajectory = rewards_sliding_window[1:, -1]
+        # discounts
+        discount_t_trajectory = jnp.zeros(r_t_trajectory.shape) + self.gamma
+        return (obs_tm1_trajectory, a_tm1_trajectory, r_t_trajectory, discount_t_trajectory, obs_t_trajectory)
+
+    def batch_sample(self, idxs):
+        obs_tm1_batch, a_tm1_batch, r_t_batch, discount_t_batch, obs_t_batch = jax.vmap(self.sample)(idxs)
+        obs_tm1_batch = obs_tm1_batch.reshape(-1, self.window_length, 1)
+        a_tm1_batch = a_tm1_batch.reshape(-1, self.window_length, 1)
+        r_t_batch = r_t_batch.reshape(-1,)
+        discount_t_batch = discount_t_batch.reshape(-1,)
+        obs_t_batch = obs_t_batch.reshape(-1, self.window_length, 1)
+        return (obs_tm1_batch, a_tm1_batch, r_t_batch, discount_t_batch, obs_t_batch)
+
+
+
+def run_loop(
+    seed: int,
+    reward_matrix: jnp.ndarray,
+    domain_randomization: bool,
+    trace: dict,
+    window_length: int,
+    use_action_history: bool = False,
+    replay_size: int = int(1e6),
+    train_episodes: int = int(1e2),
+    step_per_episode: int = 50,
+    keep_last_window_lenght_obs: bool = True,
+    gamma: float = 0.99,
+    update_every: int = 1,
+    update_after: int = 1,
+    batch_size: int = 100,
+    test_episodes: int = int(1e2),
+    domain_randomization_test: bool = True,
+    #agent?
+    #env?
+    #replay_buffer?
+
+):
+    rng = hk.PRNGSequence(seed)
+
+    # Environment
+    env = FractalEnv(reward_matrix=reward_matrix)
+    dummy_obs = []
+    dummy_action = []
+    for _ in range(window_length):
+        dummy_obs.append(env.observation_space().sample(next(rng)))
+        dummy_action.append(env.action_space().sample(next(rng)))
+    dummy_obs = jnp.asarray(dummy_obs).reshape((1, window_length, 1))
+    dummy_action = jnp.asarray(dummy_action).reshape((1, window_length, 1))
+    if use_action_history:
+        raise(NotImplementedError)
+    else:
+        dummy_obs_critic = jnp.concatenate([dummy_obs, dummy_action], axis=-1)
+
+    # Replay Buffer
+    buffer = ReplayBufferPO(
+        capacity=replay_size//window_length, 
+        episode_horizon=step_per_episode, 
+        window_length=window_length, 
+        gamma=gamma
+    )
+
+    # Agent
+    agent = GTrXLSAC(
+        rng=next(rng),
+        dummy_obs_actor = dummy_obs,
+        dummy_obs_critic = dummy_obs_critic,
+        obs_dim = 1,
+        act_dim = 3,
+        num_heads = 8, 
+        key_size = 64, 
+        num_layers = 2, 
+        dropout = 0.1, 
+        hidden_sizes_mlp = [],
+        dropouta = 0.0,
+        learning_rate = 1e-3,
+        polyak = 0.995,
+    )
+    agent_params = agent.init_params()
+    opt_states = agent.init_opt_state(agent_params)
+    memory, memory_tm1 = Memory(None, None, None, None, None), None
+
+    # Train Loop
+    start_time = time.time()
+    tot_train_ep_returns = []
+    print('Start training loop')
+    for train_episode in range(train_episodes):
+        train_ep_return = 0
+        if domain_randomization:
+            env_params = sample_params(next(rng), trace)
+        else:
+            env_params = sample_mean_params(trace)
+        obs_tm1, hs_tm1 = env.reset(next(rng), env_params)
+        if keep_last_window_lenght_obs:
+            obs_tm1_history = collections.deque(maxlen=window_length)
+        else:
+            obs_tm1_history = collections.deque(maxlen=step_per_episode)
+        for step in range(step_per_episode):
+            obs_tm1_history.append(obs_tm1)
+            a_tm1, memory_pi_tm1 = agent.get_action(
+                rng = next(rng),
+                params = agent_params,
+                obs = jnp.asarray(obs_tm1_history).reshape(1, -1, 1),
+                memory_pi_tm1 = memory.pi,
+                deterministic = False,
+            )
+            obs_t, hs_t, r_t, _, _ = env.step(next(rng), hs_tm1, obs_tm1, a_tm1, env_params)
+            train_ep_return += r_t
+            buffer.push(obs=obs_tm1, action=a_tm1, reward=r_t, episode=train_episode, step=step)
+            print(f'Episode {train_episode} Step {step}: o_tm1 {obs_tm1.round(2)}, hs_tm1 {hs_tm1}, a_tm1 {a_tm1}, r_t {r_t}')
+            obs_tm1, hs_tm1 = obs_t, hs_t
+            memory = Memory(memory_pi_tm1, None, None, None, None)
+            # Let's try without memory
+            memory, memory_tm1 = Memory(None, None, None, None, None), None
+        print(f'Episode {train_episode} total return {train_ep_return}')
+        # Update
+        if train_episode >= update_after and train_episode % update_every == 0:
+            print('Update')
+            for _ in range(update_every):
+                idxs = jax.random.randint(next(rng), shape=(batch_size,), minval=0, maxval=buffer.size_buffer)
+                batch = buffer.batch_sample(idxs)
+                agent_params, opt_states, memory = agent.update(
+                    params = agent_params,
+                    rng = next(rng),
+                    data = batch,
+                    memory = memory,
+                    opt_states = opt_states,
+                    alpha = 0.2,
+                )
+                # Let's try without memory
+                memory, memory_tm1 = Memory(None, None, None, None, None), None
+        # Collect episode return
+        tot_train_ep_returns.append(train_ep_return)
+    train_time = time.time()-start_time
+    # Save train episode returns 
+    file = 'train_rewards_GTrXL_' + 'seed'+str(seed) + '_' + time.strftime("%d-%m-%Y")+ '.pickle'
+    with open(file, "wb") as fp:
+        pickle.dump(tot_train_ep_returns, fp)
+    # Test
+    start_time = time.time() 
+    tot_test_ep_returns = []
+    print('Starting test')
+    for test_episode in range(test_episodes):
+        test_ep_return = 0
+        if domain_randomization_test:
+            env_params = sample_params(next(rng), trace)
+        else:
+            env_params = sample_mean_params(trace)
+        obs, hs = env.reset(next(rng), env_params)
+        for step in range(step_per_episode):
+            a, memory_tm1 = agent.get_action(
+                rng = next(rng),
+                params = agent_params,
+                obs = obs,
+                memory_pi_tm1 = memory_tm1,
+                deterministic = True,
+            )
+            memory_tm1 = None
+            print(f'Episode {test_episode} Step {step}: o_tm1 {obs.round(2)}, hs_tm1 {hs}, a_tm1 {a}')
+            obs, hs, r, _, _ = env.step(next(rng), hs, obs, a, env_params)
+            test_ep_return += r
+            print(f'Episode {test_episode} Step {step}: r_t {r}')
+        print(f'Episode {test_episode} total return {test_ep_return}')
+        # Collect test episode return
+        tot_test_ep_returns.append(test_ep_return)
+    test_time = time.time()-start_time
+    # Save train episode returns 
+    file = 'test_rewards_GTrXL_' + 'seed'+str(seed) + '_' + time.strftime("%d-%m-%Y")+ '.pickle'
+    with open(file, "wb") as fp:
+        pickle.dump(tot_test_ep_returns, fp)
+    # Save model
+    params_memory = {'params': agent_params, 'memory': memory}
+    file = 'model_GTrXL_' + 'seed'+str(seed) + '_' + time.strftime("%d-%m-%Y")+ '.pickle'
+    with open(file, "wb") as fp:
+        pickle.dump(params_memory, fp)
+
+        
