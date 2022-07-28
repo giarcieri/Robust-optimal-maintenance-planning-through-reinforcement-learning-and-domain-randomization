c144d3089c9817d9d5a87eacc3d52ed6ce986563
giarcieri <arcieri.1587763@studenti.uniroma1.it>
2022-07-28 14:11:41 +0200

    vmap instead of pmap

--- transformer/transformer_main.py
+++ transformer/transformer_main.py
@@ -1,6 +1,12 @@
 from .transformer_run_loop import *
 import json
+import argparse
 from functools import partial
+
+parser = argparse.ArgumentParser(description='VIBD')
+parser.add_argument('-n', '--n_seeds', type=int, metavar='',
+                    required=True, help='seed')
+args = parser.parse_args()
 
 file = 'trace.pickle'
 with open(file, "rb") as fp:
@@ -27,8 +33,11 @@ with open('transformer/config.json') as config_file:
 
 devices = jax.local_device_count()
 with open("transformer/logs.txt", "a") as f:
-    f.write(f"Running {devices} parallel seeds\n")
-seeds = jnp.broadcast_to(jnp.arange(devices), shape=(devices,2)).astype(jnp.uint32)
+    f.write(f"Running on {devices} parallel devices\n")
+seeds = int(args.n_seeds)
+with open("transformer/logs.txt", "a") as f:
+    f.write(f"Running {seeds} parallel seeds\n")
+seeds = jnp.broadcast_to(jnp.arange(seeds), shape=(seeds,2)).astype(jnp.uint32)
 run_loop_partial = partial(run_loop, trace=trace, reward_matrix=reward_matrix, **config)
-run_loop_pmap = jax.pmap(run_loop_partial, axis_name='i')
+run_loop_pmap = jax.vmap(run_loop_partial, axis_name='i')
 run_loop_pmap(seeds)
